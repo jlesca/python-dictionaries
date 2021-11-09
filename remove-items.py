71fe29b2335889b6ac2bcaeb843de63bb925ee72
José Lescano <92216756+jlesca@users.noreply.github.com>
2021-11-09 15:06:41 -0300

    Update remove-items.py

--- remove-items.py
+++ remove-items.py
@@ -1,5 +1,5 @@
-# REMOVE ITEMS FROM DICTIONARIE
-# We can remove or delete items from our dictionarie.
+# REMOVE ITEMS FROM DICTIONARY
+# We can remove or delete items from our dictionary.
 # To do this, we have to use the POP() method.
 
 print('REMOVE ITEM WITH POP()') # Display title
@@ -13,21 +13,36 @@ product = { # Open dictio
 print(product) # Display dictio
 
 product.pop('genre') # Remove the specified item with pop() method. In this case, 'genre' was removed.
-print(product) # Display 
+print(product) # Display dictio updated 
 input()
 
 
 # Also we can remove the last item inserted with POPITEM() method.
 
-print('REMOVE LAST ITEM INSERTED')
-product = {
+print('REMOVE LAST ITEM INSERTED') # Display title
+product = { # Open dictio
     'art' : 'book',
     'code' : 1234,
     'price' : 2.5,
     'genre' : 'fiction'
-}
+} # Close dictio
 
-print(product)
+print(product) # Display dictio
 
-product.pop('genre')
-print(product)
+product.pop('genre') # Remove the last inserted item. In this case 'genre'
+print(product) # Display dictio updated
+input()
+
+# CLEAR DICTIONARY
+# If you wish clear the dictionary complete, can use CLEAR() method.
+
+print('CLEAR DICTIONARY') # Display title
+product.clear() # Clear items from dictio
+print(product) # Display dictio updated. In this case it will be '{}'
+input()
+
+# DELETING DICTIO
+# You can to delete completely the dictionary with DEL keyword.
+
+del product # Deleting dictio
+print(product) # Display dictio, but will be an error. NameError: name 'product' is not defined
